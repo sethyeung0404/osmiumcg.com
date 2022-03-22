974e22588675e90ffbf2fd1a66084e0f51f78b30
sethyeung0404 <62109034+sethyeung0404@users.noreply.github.com>
2022-03-22 22:41:30 +0800

    changed font 	modified:   components/Text/SubPageParagraph2.js 	modified:   pages/_document.js 	modified:   styles/style.css

--- components/Text/SubPageParagraph2.js
+++ components/Text/SubPageParagraph2.js
@@ -3,7 +3,7 @@ import React from 'react'
 function SubPageParagraph2({ Text }) {
   return (
     <div>
-      <p className="pb-3 text-justify text-base font-light">
+      <p className="pb-3 text-justify text-base font-light leading-6">
         {Text}
         <br />
       </p>
--- pages/_document.js
+++ pages/_document.js
@@ -8,11 +8,7 @@ export default function Document() {
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
         <link
-          rel="stylesheet"
-          href="https://fonts.googleapis.com/css2?family=Noto+Sans+SC&family=Noto+Sans+TC&display=swap"
-        />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap"
+          href="https://fonts.googleapis.com/css2?family=Nunito+Sans&Noto+Sans+SC&family=Noto+Sans+TC&family=Roboto&display=swap"
           rel="stylesheet"
         />
       </Head>
