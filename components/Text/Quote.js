6e2ec85d57bdc4f88f0a956ae1565921faba0c02
sethyeung0404 <62109034+sethyeung0404@users.noreply.github.com>
2022-03-28 16:38:14 +0800

    added Trans to sustainability as well 	modified:   components/Text/Quote.js 	modified:   components/Text/SubPageHeading2.js 	modified:   pages/about-us/sustainability.js 	modified:   public/locales/en-US/about-us.json 	modified:   public/locales/ja/about-us.json     modified:   public/locales/zh-CN/about-us.json 	modified:   public/locales/zh-HK/about-us.json

--- components/Text/Quote.js
+++ components/Text/Quote.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
-function Quote() {
-  return <div>Quote</div>
+function Quote({ Text }) {
+  return <div className="text-center text-green-600 px-3 pt-2 pb-6 text-2xl">{Text}</div>
 }
 
 export default Quote
